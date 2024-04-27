46ad0a1b8643a9784a80a7fbd1660ed5e57ccf69
vk3305 <54815422+vk3305@users.noreply.github.com>
2024-04-27 09:19:29 -0700

    Update streamlit_app.py
    New session to display fruityvice nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your smoothie is ordered!, {name_on_order}")
     
+# New session to display fruityvice nutrition information.
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
